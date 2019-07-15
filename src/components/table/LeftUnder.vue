3e2cd3a71834e7a238dcd9ee7b20ffdebe70644b
dong <512595258@qq.com>
2019-07-15 08:29:11 +0800

    标签引导线隐藏

--- src/components/table/LeftUnder.vue
+++ src/components/table/LeftUnder.vue
@@ -42,63 +42,142 @@ export default{
                 {
                     name:'访问来源',
                     type:'pie',
-                    minShowLabelAngle:8,
+                    // minShowLabelAngle:300,
                     // hoverOffset:2,
                     hoverAnimation:false,
                     radius: ['23%', '35%'],
-                    avoidLabelOverlap: false,
+                    avoidLabelOverlap: true,
                     
                     //文本标签
                     label: {
-                        normal: {
-                            show: true,
-                            // show:function(){
-                            //     if(data.percent>10){
-                            //         return ture
-                            //     }
-                            // },
-                            position:'outer',
-                            borderWidth:2,
-                            borderRadiu:2,
-                            padding:[2,2],
-                            lineStyle:{
-                                color:'#333'
-                            },
-                            formatter:function(data){
-                                console.log(data)
-                                return data.name +" "+ data.percent.toFixed(0)+"%"
-                            }
-
-                            // formatter:'{b} {d}%'
+                        
+                        show: true,
+                        position:'outside',
+                        borderWidth:2,
+                        borderRadiu:2,
+                        padding:[2,2],
+                        lineStyle:{
+                            color:'#333'
                         },
-                        emphasis: {
-                            show: true,
-                            textStyle: {
-                                fontSize: '12',
-                                fontWeight: 'bold'
+                        // formatter:'{b} {d}%'
+                        formatter:function(data){
+                            // console.log(data)
+                            if(data.percent>8){
+                                return data.name +" "+ data.percent.toFixed(0)+"%"
+                            }else{
+                                return data.name=""
+                                // return data.labelLine:{show:""}
                             }
                         }
+                    },   
+                    //高亮扇区 
+                    emphasis: {
+                        show: true,
+                        textStyle: {
+                            fontSize: '12',
+                            fontWeight: 'bold'
+                        }
                     },
+                
                     //文本标签引导线
                     labelLine: {
-                        normal: {
-                            show:true,
-                            length:5,
+                            // formatter:function(data){
+                            //     if(data.percent>8){
+                            //         return {show:true}
+                            //     }else{
+                            //         return {show:false}
+                                    
+                            //     }
+                            // },
+                            show:false,
+                            length:10,
+                            length2:10,
                             fontWeight:8,
-                        }
+                            
+                        
                     },
                     data:[
-                        {value:60, name:'0769'},
-                        {value:90, name:'3D      '},
-                        {value:100, name:'歌蒂娅'},
-                        {value:150, name:'凯奇'},
-                        {value:120, name:'助眠'},
-                        {value:200, name:'V6'},
-                        {value:300, name:'0760'},
-                        {value:200, name:'PAULY'},
-                        {value:100, name:'HOME'},
-                        {value:10, name:'国际'},
-                        {value:10, name:'兰博基尼'},
+                        {value:260, name:'0769',
+                            labelLine:{
+                                // formatter:function(data){
+                                //     if(data.percent>8){
+                                //         return {show:false}
+                                //     }else{
+                                //         return {show:true}
+                                        
+                                //     }
+                                // }
+                                show:true
+                            }
+                              
+                              
+                        },
+                        {value:90, name:'3D      ',
+                              labelLine:{
+                                  show:""
+                                  } 
+                              
+                              },
+                        {value:100, name:'歌蒂娅',
+                                labelLine:{
+                                  show:""
+                              
+                              }                        
+                        },
+                        {value:150, name:'凯奇',
+                                labelLine:{
+                                        show:"" 
+                                        } 
+                              
+                        },
+                        {value:120, name:'助眠',
+                                labelLine:{
+                                    formatter:function(data){
+                                            if(data.percent>8){
+                                                return {show:false}
+                                            }else{
+                                                return {show:true}
+                                            
+                                            }
+                                }
+                                    } 
+                              
+                        },
+                        {value:200, name:'V6',
+                                labelLine:{
+                                  show:""
+                                  } 
+                              
+                              
+                        },
+                        {value:300, name:'0760',
+                               labelLine:{
+                                  show:""
+                                  } 
+                              
+                              
+                        },
+                        {value:200, name:'PAULY',
+                                 labelLine:{
+                                  show:""
+                                  } 
+                        },
+                        {value:100, name:'HOME',
+                                  labelLine:{
+                                  show:""
+                                  }                       
+                        },
+                        {value:10, name:'国际',
+                                   labelLine:{
+                                  show:""
+                                  } 
+                        },
+                        {value:10, name:'兰博基尼',
+                                  labelLine:{
+                                  show:""
+                                  }                       
+                        },
+                        
                     ]
                 }
             ],
