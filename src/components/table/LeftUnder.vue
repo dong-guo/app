82461b0faac6e5d7e8bc4d02c02bb92c0ed06863
dong <512595258@qq.com>
2019-07-20 14:43:55 +0800

    -m左下

--- src/components/table/LeftUnder.vue
+++ src/components/table/LeftUnder.vue
@@ -6,182 +6,134 @@
 
 <script>
 
+import axios from 'axios' 
 var echarts = require('echarts')
 
 export default{
   name:'LeftUnder',
   data(){
-    return{
-        page31:("床垫销售额"),
-        page32:("床品销售额"),
-        page33:("床架销售额")
-    }   
-  },
-  mounted() {
-        // 基于准备好的dom，初始化echarts实例
-        var myChart = echarts.init(document.getElementById('LeftUnder'));
-        // var data = [];
-        // arr.push({value:260, name:'0769',labelLine:{show:true}});
-        // arr.push({value:90, name:'3D      ',labelLine:{show:true}});
-        // arr.push({value:100, name:'歌蒂娅',labelLine:{show:true}});
-        // arr.push({value:150, name:'凯奇',labelLine:{show:true}});
-        // arr.push({value:120, name:'助眠',labelLine:{show:true}});
-        // arr.push({value:200, name:'V6', labelLine:{show:true}});
-        // arr.push({value:300, name:'0760',labelLine:{show:true}});
-        // arr.push({value:200, name:'PAULY',labelLine:{show:true}});
-        // arr.push({value:100, name:'HOME',labelLine:{show:true}});
-        // arr.push({value:10, name:'国际',labelLine:{show:true}});
-        // arr.push({value:10, name:'兰博基尼',labelLine:{show:true}});
-        // arr.sort(function(a,b){
-        //     if(a.value<b.value){
-        //         return -1
-        //     }else if(a.value>b.value){
-        //         return 1;
-        //     }
-        //     return 0;
-        // });
-        // for(var i=0;i<arr.length;i++){
-        //     console.log(data[i])
-        // }
-        myChart.setOption ({
-            tooltip: { 
-               trigger: 'none',
-               formatter: "{a} <br/>{b}:{c}({d}%)"
-            },
-            //标题
-            legend: {
-                orient: 'horizontal',
-                align:'left',
-                y: 'bottom',
-                icon: "circle",
-                itemWidth:10,
-                itemHeight:10,
-                itemGap:15,
-                textStyle:{
-                    color:'rgba(187,197,220,1)',
-                    fontSize:14,
+        return{
+           moons:[],
+        }   
+    },
+    created() {
+		
+    },
+    mounted() {
+        this.getData()
+    },
+    methods: {
+        getData() {
+            //请求数据
+            console.log(111)
+            this.$axios.get('https://mobiletest.derucci.net/consumer-admin/api/sales/v1/brand/sales/proportion')
+            .then(res => {
+                console.log(222)
+                let data = res.data.data
+                console.log('请求的数据', data)
+                var myChart = echarts.init(document.getElementById('LeftUnder'))
+                let option = this.createOption()
+                console.log(123, option)
+                myChart.setOption(option)
+			})
+			.catch(error => {
+				console.log(error);
+			})
+        },
+        createOption(){
+            return {
+                tooltip: { 
+                trigger: 'none',
+                formatter: "{a} <br/>{b}:{c}({d}%)"
                 },
-                data:['0769','3D      ','歌蒂娅','凯奇','助眠','V6','0760','PAULY','HOME','国际','兰博基尼']
-            },
-            series: [
-                {
-                    name:'访问来源',
-                    type:'pie',
-                    // minShowLabelAngle:300,
-                    // hoverOffset:2,
-                    clockwise:false,
-                    starAngle:90,
-                    hoverAnimation:false,
-                    radius: ['35%', '50%'],
-                    avoidLabelOverlap: true,
-                    
-                    //文本标签
-                    label: {
-                        
-                        show: true,
-                        position:'outside',
-                        borderWidth:2,
-                        borderRadiu:2,
-                        padding:[2,2],
-                        lineStyle:{
-                            color:'#333'
-                        },
-                        // formatter:'{b} {d}%'
-                        formatter:function(data){
-                            // console.log(data)
-                            if(data.percent>10){
-                                return data.name +" "+ data.percent.toFixed(0)+"%"
-                            }else{
-                                return data.name=""
-                                // return data.labelLine:{show:""}
-                            }
-                        }
-                    },   
-                    //高亮扇区 
-                    emphasis: {
-                        show: true,
-                        textStyle: {
-                            fontSize: '12',
-                            fontWeight: 'bold'
-                        }
+                //标题
+                legend: {
+                    orient: 'horizontal',
+                    align:'left',
+                    y: 'bottom',
+                    icon: "circle",
+                    itemWidth:10,
+                    itemHeight:10,
+                    itemGap:15,
+                    textStyle:{
+                        color:'rgba(187,197,220,1)',
+                        fontSize:14,
                     },
-                
-                    //文本标签引导线
-                    labelLine: {
-                            show:true,
-                            length:20,
-                            length2:20,
-                            fontWeight:8,
-                    },
-                    // data:this.data
-                    data:[
-                        {value:300, name:'0769',
-                            itemStyle:{color:'rgba(169,89,255,1)'},
-                            labelLine:{show:true}
-                        },
-                        {value:1, name:'3D      ',
-                            itemStyle:{color:'rgba(103,89,255,1)'},
-                            labelLine:{show:false}
+                    data: ['0769','3D','歌蒂娅']
+                    // data:this.moons.name,
+                },
+                series: [
+                    {
+                        name:'访问来源',
+                        type:'pie',
+                        // minShowLabelAngle:300,
+                        // hoverOffset:2,
+                        clockwise:false,
+                        starAngle:90,
+                        hoverAnimation:false,
+                        radius: ['35%', '50%'],
+                        avoidLabelOverlap: true,
                         
-                        },
-                        {value:1, name:'歌蒂娅',
-                            itemStyle:{color:'rgba(89,164,255,1)'},
-                            labelLine:{show:false}
-                        
-                        },
-                        {value:160, name:'凯奇',
-                            itemStyle:{color:'rgba(89,241,255,1)'},
-                            labelLine:{show:true}
-                        
-                        },
-                        {value:1, name:'助眠',
-                            itemStyle:{color:'rgba(169,89,208,1)'},
-                            labelLine:{show:false}
-                        
-                        }, 
-                        {value:1, name:'V6',
-                            itemStyle:{color:'rgba(89,255,120,1)'},
-                            labelLine:{show:false}
-                        
-                        },
-                        {value:300, name:'0760',
-                            itemStyle:{color:'rgba(103,89,255,1)'},
-                            labelLine:{show:true}
-                        
-                        },
-                        {value:1, name:'PAULY',
-                            itemStyle:{color:'rgba(203,255,89,1)'},
-                            labelLine:{show:false}
-                        
-                        },
-                        {value:100, name:'HOME',
-                            itemStyle:{color:'rgba(203,255,89,1)'},
-                            labelLine:{show:true}
-                        
-                        },
-                        {value:1, name:'国际',
-                            itemStyle:{color:'rgba(255,200,89,1)'},
-                            labelLine:{show:false}
-                        
-                        },
-                        {value:120, name:'兰博基尼',
-                            itemStyle:{color:'rgba(255,100,89,1)'},
-                            labelLine:{show:true}
-                        },  
+                        //文本标签
+                        label: {
                             
-                    ],
+                            show: true,
+                            position:'outside',
+                            borderWidth:2,
+                            borderRadiu:2,
+                            padding:[2,2],
+                            lineStyle:{
+                                color:'#333'
+                            },
+                            formatter:'{b} {d}%'
+                        },   
+                        //高亮扇区 
+                        emphasis: {
+                            show: true,
+                            textStyle: {
+                                fontSize: '12',
+                                fontWeight: 'bold'
+                            }
+                        },
                     
-                }
-            ],
-            grid:[{
-                left:'10%',
-                bottom:'20%',
-                top:'12%',
-                right:'10%'
-            }]
+                        //文本标签引导线
+                        labelLine: {
+                                show:false,
+                                length:20,
+                                length2:20,
+                                fontWeight:8,
+                        },
+                        // data:this.data
+                        data:[
+                            {
+                                value:300, name:'0769',
+                                itemStyle:{color:'rgba(169,89,255,1)'},
+                            },
+                            {
+                                value:1, name:'3D      ',
+                                itemStyle:{color:'rgba(103,89,255,1)'},
+                            
+                            },
+                            {
+                                value:1, name:'歌蒂娅',
+                                itemStyle:{color:'rgba(89,164,255,1)'},
+                            }
+                                
+                        ],
+                        // data:this.moons
+                    }
+                ],
+                grid:[{
+                    left:'10%',
+                    bottom:'20%',
+                    top:'12%',
+                    right:'10%'
+                }]
 
-        })
-   } 
+            }
+        },
+        mounted() {},
+    },
 }
 </script>
 
--- src/components/table/RightUnder.vue
+++ src/components/table/RightUnder.vue
@@ -16,163 +16,270 @@ export default{
     name:'RightUnder',
     data(){
         return{
-            movies:[]
+            // movies:[],
             
-        }   
+        };   
     },
-    created(){
-        this.$axios.get("https://mobiletest.derucci.net/consumer-admin/api/sales/v1/product/list")
-        .then(res=>{
-            this.filterData(res.data);
-            console.log(this.movies)    
-        }) 
-        .catch(error=>{
-            console.log(error);
-        })  
-    },
+    created() {
+		this.$axios
+			.get('https://mobiletest.derucci.net/consumer-admin/api/sales/v1/product/list')
+			.then(res => {
+				this.filterData(res.data);
+				console.log(this.movies);
+			})
+			.catch(error => {
+				console.log(error);
+			});
+	},
     methods:{
-     filterData(data){
-         var pul = [];
-         for (var i=0; i<data.data.length;i++){
-                var moviesObj={
-                    model:data.data[i].model,
-                    qty:data.data[i].qty
-                }
-                pul.push(moviesObj);
+        filterData(data){
+            var modelAry = [];
+            var qtyAry = [];
+            for (var i = 0; i < data.data.length; i++){
+                modelAry.push(data.data[i].model)
+                qtyAry.push((data.data[i].qty).toFixed(0))  
             }
-         this.movies=pul;
-        //  console.log(typeof(movies))
-     }
-  },
-    mounted() {
- //实验--------------------------------------------------
-        
-        fetchData(function (data) {
+            this.movies={
+                model:modelAry,
+                qty:qtyAry,
+            };
+            this.createMap();
+             console.log(typeof(movies))
+        },
+        createMap(){
+            // 基于准备好的dom，初始化echarts实例
+            var myChart = echarts.init(document.getElementById('RightUnder')); 
             myChart.setOption({
-                yAxis: {
-                    data: this.movies.model
+                title: {
+                    text: '',
+                    subtext: ''
                 },
-                series: [{
-                    name: '(单位：万元)',
-                    data: this.movies.qty
-                }]
-            });
-        })
-//实验--------------------------------------------------  
-        // 基于准备好的dom，初始化echarts实例
-        var myChart = echarts.init(document.getElementById('RightUnder')); 
-        myChart.setOption({
-            title: {
-                text: '',
-                subtext: ''
-            },
-            tooltip: {
-                trigger: 'axis',
-                axisPointer: {
-                    type: 'shadow'
-                },
-                label:{
-                    show:false
-                }
-            },
-            legend: {
-                data: [ '(单位：万元)'],
-                icon:'none',
-                show:true,
-                textStyle:{
-                    color:'white',
-                    fontSize:14
-                },
-                left:'110',
-                top:'15'
-            },
-           
-            xAxis: {
-                type: 'value',
-                show:true,
-                boundaryGap: [0, 0.01],
-            
-                axisLine:{
-                    show:false
-                },
-                axisTick:{
-                    show:false
-                },
-                axisLabel:{
-                    color:'white'
-                },
-                //X分隔线
-                splitLine:{
-                    show:true,
-                    lineStyle:{
-                        color:'rgba(47,95,233,1)',
-                        opacity:0.4,
-                        width:1
+                tooltip: {
+                    trigger: 'axis',
+                    axisPointer: {
+                        type: 'shadow'
                     },
-                    type:'dotted'
-                }
-            },
-            
-            yAxis: {
-                type: 'category',
-                nameTextStyle:{
-                    color:'blue',
-                    fontSize:1,
-                },
-                axisTick:{
-                    show:false,
-                    color:'green',
-                    lineStyle:{
-                        color:'blue'
+                    label:{
+                        show:false
                     }
                 },
-                axisLabel:{
+                legend: {
+                    data: [ '(单位：万元)'],
+                    icon:'none',
                     show:true,
-                    fontWeight:'normal',
-                    fontSize:14
-                }, 
-                axisLine:{
-                    show:false,
-                    lineStyle:{
+                    textStyle:{
                         color:'white',
-                        width:1,
-                        fontSize:18,
+                        fontSize:14
+                    },
+                    left:'110',
+                    top:'15'
+                },
+            
+                xAxis: {
+                    type: 'value',
+                    show:true,
+                    boundaryGap: [0, 0.01],
+                
+                    axisLine:{
+                        show:false
+                    },
+                    axisTick:{
+                        show:false
+                    },
+                    axisLabel:{
+                        color:'white'
+                    },
+                    //X分隔线
+                    splitLine:{
+                        show:true,
+                        lineStyle:{
+                            color:'rgba(47,95,233,1)',
+                            opacity:0.4,
+                            width:1
+                        },
                         type:'dotted'
                     }
-                    
                 },
                 
-                // data: ['BCQ2-060','BCQ2-004','BCQ2-003','BCQ2-002','BCQ2-012','BCQ2-010','BCQ2-060','BCQ2-004','BCQ2-003','BCQ2-002']
-                data:this.movies.model
-            },
-            series: [
+                yAxis: {
+                    type: 'category',
+                    nameTextStyle:{
+                        color:'blue',
+                        fontSize:1,
+                    },
+                    axisTick:{
+                        show:false,
+                        color:'green',
+                        lineStyle:{
+                            color:'blue'
+                        }
+                    },
+                    axisLabel:{
+                        show:true,
+                        fontWeight:'normal',
+                        fontSize:14
+                    }, 
+                    axisLine:{
+                        show:false,
+                        lineStyle:{
+                            color:'white',
+                            width:1,
+                            fontSize:18,
+                            type:'dotted'
+                        }
+                        
+                    },
+                    
+                    // data: ['BCQ2-060','BCQ2-004','BCQ2-003','BCQ2-002','BCQ2-012','BCQ2-010','BCQ2-060','BCQ2-004','BCQ2-003','BCQ2-002']
+                    data: this.movies.model,
+                },
+                series: [
+                    
+                    {
+                        name: '(单位：万元)',
+                        type: 'bar',
+                        label:{
+                        show:true,
+                        position:'right',
+                        color:'white',
+                        },
+                        itemStyle:{
+                        color:'blue',
+                        barBorderRadius:10,
+                        },
+                        barWidth:10,
+                        // data: [2800, 3000, 3200, 3500, 3800, 4000,4200,4300,4700,5000]
+                        data: this.movies.qty,
+                    }
+                ],
+                grid:[{
+                    left:'23%',
+                    bottom:'10%',
+                    top:'18%',
+                    right:'10%'
+                }]
+            })
+        }
+
+        
+    },    
+        mounted() { 
+            // 基于准备好的dom，初始化echarts实例
+            // var myChart = echarts.init(document.getElementById('RightUnder')); 
+            // myChart.setOption({
+            //     title: {
+            //         text: '',
+            //         subtext: ''
+            //     },
+            //     tooltip: {
+            //         trigger: 'axis',
+            //         axisPointer: {
+            //             type: 'shadow'
+            //         },
+            //         label:{
+            //             show:false
+            //         }
+            //     },
+            //     legend: {
+            //         data: [ '(单位：万元)'],
+            //         icon:'none',
+            //         show:true,
+            //         textStyle:{
+            //             color:'white',
+            //             fontSize:14
+            //         },
+            //         left:'110',
+            //         top:'15'
+            //     },
+            
+            //     xAxis: {
+            //         type: 'value',
+            //         show:true,
+            //         boundaryGap: [0, 0.01],
                 
-                {
-                    name: '(单位：万元)',
-                    type: 'bar',
-                    label:{
-                       show:true,
-                       position:'right',
-                       color:'white',
-                    },
-                    itemStyle:{
-                       color:'blue',
-                       barBorderRadius:10,
-                    },
-                    barWidth:10,
-                    // data: [2800, 3000, 3200, 3500, 3800, 4000,4200,4300,4700,5000]
-                    data:this.movies.qty
-                }
-            ],
-            grid:[{
-                left:'23%',
-                bottom:'10%',
-                top:'18%',
-                right:'7%'
-            }]
-        })
-   } 
+            //         axisLine:{
+            //             show:false
+            //         },
+            //         axisTick:{
+            //             show:false
+            //         },
+            //         axisLabel:{
+            //             color:'white'
+            //         },
+            //         //X分隔线
+            //         splitLine:{
+            //             show:true,
+            //             lineStyle:{
+            //                 color:'rgba(47,95,233,1)',
+            //                 opacity:0.4,
+            //                 width:1
+            //             },
+            //             type:'dotted'
+            //         }
+            //     },
+                
+            //     yAxis: {
+            //         type: 'category',
+            //         nameTextStyle:{
+            //             color:'blue',
+            //             fontSize:1,
+            //         },
+            //         axisTick:{
+            //             show:false,
+            //             color:'green',
+            //             lineStyle:{
+            //                 color:'blue'
+            //             }
+            //         },
+            //         axisLabel:{
+            //             show:true,
+            //             fontWeight:'normal',
+            //             fontSize:14
+            //         }, 
+            //         axisLine:{
+            //             show:false,
+            //             lineStyle:{
+            //                 color:'white',
+            //                 width:1,
+            //                 fontSize:18,
+            //                 type:'dotted'
+            //             }
+                        
+            //         },
+                    
+            //         data: ['BCQ2-060','BCQ2-004','BCQ2-003','BCQ2-002','BCQ2-012','BCQ2-010','BCQ2-060','BCQ2-004','BCQ2-003','BCQ2-002']
+            //         // data: this.movies.model,
+            //     },
+            //     series: [
+                    
+            //         {
+            //             name: '(单位：万元)',
+            //             type: 'bar',
+            //             label:{
+            //             show:true,
+            //             position:'right',
+            //             color:'white',
+            //             },
+            //             itemStyle:{
+            //             color:'blue',
+            //             barBorderRadius:10,
+            //             },
+            //             barWidth:10,
+            //             data: [2800, 3000, 3200, 3500, 3800, 4000,4200,4300,4700,5000]
+            //             // data: this.movies.qty,
+            //         }
+            //     ],
+            //     grid:[{
+            //         left:'23%',
+            //         bottom:'10%',
+            //         top:'18%',
+            //         right:'7%'
+            //     }]
+            // })
+        } 
+        
 }
+
 </script>
 
 
