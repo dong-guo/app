7e98de9c401c460b308fa172565fa8bd132dc363
mangoguang <542436458@qq.com>
2019-07-26 02:33:19 +0800

    处理地图数据。

--- src/components/World.vue
+++ src/components/World.vue
@@ -8,6 +8,7 @@
 <script>
 import axios from 'axios'
 import echarts from 'echarts'
+import leftTopIcon from '../base64/leftTop'
 export default {
   name: 'world',
   props:{
@@ -25,96 +26,78 @@ export default {
     axios.get('./geoJson/world.json').then((res) => {
       let  worldJson = res.data
       axios.get('https://mobiletest.derucci.net/consumer-admin/api/sales/v1/national/sales/amount').then((res)=>{
-        console.log('请求地图数据',worldJson)
-        let data =res.data.data
-        console.log('数据得到',data)
-        let worldData = [];
-        for(var i =0; i < data.length; i++){
-            let amount = data[i].amount;
-            let state = data[i].state;
-            worldData.push({
-              "name":state,
-              "value":amount,
-              "label":{
-                show:true,
-              },
-              symbolSize:[300,120],
-              label:{
-                color:'white',
-                show:true,
-                position:'insideleft',
-                offset:[30,15],
-                fontSize:18,
-              },
-            })
-        }
-        console.log("地图数组",worldData);
+        // console.log('请求地图数据',worldJson)
+        // let data =res.data.data
+        // console.log('数据得到',data)
+        // let worldData = [];
+        // for(var i =0; i < data.length; i++){
+        //     let amount = data[i].amount;
+        //     let state = data[i].state;
+        //     worldData.push({
+        //       "name":state,
+        //       "value":amount,
+        //       "label":{
+        //         show:true,
+        //       },
+        //       symbolSize:[300,120],
+        //       label:{
+        //         color:'white',
+        //         show:true,
+        //         position:'insideleft',
+        //         offset:[30,15],
+        //         fontSize:18,
+        //       },
+        //     })
+        // }
+        // console.log("地图数组",worldData);
         echarts.registerMap('world', worldJson,);
         this.myEchart = echarts.init(document.getElementById("world"));
+        console.log(123123, leftTopIcon)
         let option = {
-            series: {
-              type: 'map',
-              data:[],
-              nameMap:{
-                  'china':'中国',
-                  'canada':'加拿大'
-                },     
-              top: 50,
-              left:90,
-              zoom: 1.2,
-              map: 'world',
-              itemStyle:{
-                areaColor:'rgba(32,63,158,1)',
-                borderWidth:0,
-              },
-              emphasis:{
-                label:{
-                  show:true,
-                  color:'white',
-                  fontWeight:500,
-                  fontSzie:18,
-                  rich:{},
-                },
-                itemStyle:{
-                  areaColor:'rgba(89,164,255,1)',
-                }
-              },
-              label:{
-                fontSize:20,
-                //  backgroundColor:{
-                //      image:'../assets/images/左上@2x.png'
-                //  },
-                //  width:197,
-                //  height:100,
-                //  rich:{
-                // //     // backgroundColor:{
-                // //     //   //  image:'../assets/images/左上@2x.png'
-                // //     // },
-                //  }
-              },
-              markPoint:{
-                // symbol:'image://../assets/images/左上@2x.png',
-                
-                // symbolOffset:[0,0],
-                // showAllSymbol:true,
-                // symbolSize:[100,170],
-                emphasis:{
-                    label:{
-                      formatter:'{b}:{c}',
-                      // show:true,
-                    }
-                },
-                data:[]
-                         
-                      
-              },
-              
-            },
-            grid:{
-              left:'0%',
-              top:'0%'
+          geo: {
+            type: 'map',
+            map: 'world',
+            zoom: 1,
+            // roam: true,
+            top: 0,
+            left: 0,
+            right: 0,
+            itemStyle:{
+              areaColor:'rgba(32,63,158,1)',
+              borderWidth:0,
             }
-          };
+          },
+          series: [
+            {
+              coordinateSystem: 'geo',
+              type: "scatter",
+              symbol: `image://${leftTopIcon}`,
+              symbolSize: [173, 80],
+              data:[
+                {
+                  name: '中国:187526',
+                  value: [113.5, 63.48],
+                  label: {
+                    show: true,
+                    position: [30, 10],
+                    color: '#fff',
+                    formatter: `{b}`
+                  }
+                },
+                {
+                  name: '澳大利亚:1864',
+                  value: [135.13, -18.3],
+                  label: {
+                    show: true,
+                    position: [30, 10],
+                    color: '#fff',
+                    formatter: `{b}`
+                  }
+                }
+              ],   
+            }
+          ]
+        };
          // var  myChart = echarts.init(document.getElementById('main'));
          this.myEchart.setOption(option); 
       })    
